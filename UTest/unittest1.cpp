9877bf868edd87dafe1a7ee4a7356e9f9a9a9aec
Gabriel <gaboico@gmail.com>
2022-10-30 19:08:48 -0400

    Updating tests

--- UTest/unittest1.cpp
+++ UTest/unittest1.cpp
@@ -11,7 +11,7 @@ namespace UTest
 	{
 	public:
 		
-		TEST_METHOD(TestMethod1)
+		TEST_METHOD(Test_method1)
 		{
 			
 			vector<int> arr = {1,2,3};
@@ -27,7 +27,7 @@ namespace UTest
 			}
 		}
 
-		TEST_METHOD(TestMethod2)
+		TEST_METHOD(Test_method2)
 		{
 
 			vector<int> arr = { 3,2,1 };
@@ -44,7 +44,7 @@ namespace UTest
 			}
 		}
 
-		TEST_METHOD(TestMethod3)
+		TEST_METHOD(Test_method3)
 		{
 
 			vector<int> arr = { 5,5,6 };
@@ -61,7 +61,7 @@ namespace UTest
 			}
 		}
 
-		TEST_METHOD(TestMethod4)
+		TEST_METHOD(Test_method4)
 		{
 
 			vector<int> arr = { 1,1,1 };
@@ -70,6 +70,42 @@ namespace UTest
 			try {
 				permut->get_Next_Permt(arr);
 				cout << arr[0] << " " << arr[1] << " " << arr[2];
+			}
+			catch (invalid_argument& e)
+			{
+				cerr << e.what() << endl;
+
+			}
+
+		}
+
+		TEST_METHOD(Test_method5)
+		{
+
+			vector<int> arr = { 0,0,0 };
+			permutation*  permut = new permutation();
+
+			try {
+				permut->get_Next_Permt(arr);
+				cout << arr[0] << " " << arr[1] << " " << arr[2];
+			}
+			catch (invalid_argument& e)
+			{
+				cerr << e.what() << endl;
+
+			}
+
+		}
+
+		TEST_METHOD(Test_method6)
+		{
+
+			vector<int> arr = { 1,3,5,7,9 };
+			permutation*  permut = new permutation();
+
+			try {
+				permut->get_Next_Permt(arr);
+				cout << arr[0] << " " << arr[1] << " " << arr[2] << " " << arr[3]<< " " << arr[4];
 			}
 			catch (invalid_argument& e)
 			{
